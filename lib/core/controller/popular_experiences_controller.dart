c608cf9505cab7d0f09d983775f09ab1f753b732
alireza <farmanialireza668@gmail.com>
2025-05-23 16:04:27 +0330

    create controller for PopularExperiences

--- lib/core/controller/popular_experiences_controller.dart
+++ lib/core/controller/popular_experiences_controller.dart
@@ -1,3 +1,20 @@
 import 'package:get/get.dart';
+import 'package:project2/core/modules/popular_experiences_module.dart';
 
-class PopularExperiencesController extends GetxController {} 
+class PopularExperiencesController extends GetxController {
+  var distance = <PopularExperiencesModule>[].obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchDistance();
+  }
+
+  void fetchDistance() {
+    distance.value = [
+      PopularExperiencesModule(id: 1, images: "", name: "Sea Diving", distance: "1.1"),
+      PopularExperiencesModule(id: 2, images: "", name: "Mountain Trip", distance: "5.1"),
+      PopularExperiencesModule(id: 3, images: "", name: "Beach Adventure", distance: "6"),
+    ];
+  }
+}
--- lib/core/modules/popular_experiences_module.dart
+++ lib/core/modules/popular_experiences_module.dart
@@ -1,10 +1,12 @@
 // create a module for PopularExperiences
 class PopularExperiencesModule {
+  int id;
   String images;
   String name;
   String  distance;
 
   PopularExperiencesModule({
+    required this.id,
     required this.images,
     required this.name,
     required this.distance
