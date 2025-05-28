dff48e7a11358ccb26d5c21a024fa1651b328977
alireza <farmanialireza668@gmail.com>
2025-05-28 20:19:28 +0330

    upgrade flutter sdk an packages

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -48,7 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
             // show category list
             CategoryWidget(),
             // show popular exprience list
-            PopularExperiencesWidget()
+            PopularExperiencesWidget(),
           ],
         ),
       ),
